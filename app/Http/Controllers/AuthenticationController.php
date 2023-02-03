fdc9301929617daa29eaf4a48c77bd43f088bca1
MatijaBojanic <matija.bojanic@gmail.com>
2023-02-03 22:46:48 +0100

    Added login & show route for user to validate sanctum functionality

---
+++ app/Http/Controllers/AuthenticationController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticationController extends Controller
+{
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (! Auth::attempt($credentials)) {
+            return redirect()->back()->with('error', 'Invalid credentials');
+        }
+
+        return response()->json('success');
+    }
+}
