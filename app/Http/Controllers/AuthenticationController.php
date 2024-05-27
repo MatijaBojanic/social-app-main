4de88600942799723a30571a187321aae8e78056
MatijaBojanic <matija.bojanic@gmail.com>
2024-05-27 21:49:49 +0200

    permission issue resolved

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -6,6 +6,7 @@ use App\Events\UserLoggedInEvent;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthenticationController extends Controller
 {
