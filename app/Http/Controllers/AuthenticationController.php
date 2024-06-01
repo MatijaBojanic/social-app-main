7e6bd339be2a343e9396613770724d240eddf4f9
MatijaBojanic <matija.bojanic@gmail.com>
2024-06-01 13:53:32 +0200

    Cors error fixed. Auth properly throwing errors

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -6,6 +6,7 @@ use App\Events\UserLoggedInEvent;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthenticationController extends Controller
 {
@@ -19,8 +20,9 @@ class AuthenticationController extends Controller
         $credentials = $request->only('email', 'password');
 
         if (! Auth::attempt($credentials)) {
-            logger('Failed to login?');
-            return response()->json('Invalid credentials');
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
         }
 
         event(new UserLoggedInEvent(Auth::user()));
