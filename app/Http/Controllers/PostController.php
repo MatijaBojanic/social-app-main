f8e6add332bea17106bf1ff8a785e93e670bd424
MatijaBojanic <matija.bojanic@gmail.com>
2024-06-01 16:27:21 +0200

    Showing a post and its comments

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -42,7 +42,7 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        return $post->load('comments');
+        return $post->load(['user', 'comments.user']);
     }
 
     /**
